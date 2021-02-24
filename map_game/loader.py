8f6c56bae7a32139d427de586580d41c5878a028
Student <123>
2021-02-24 19:39:25 +0400

    Исправили функцию load. Исправили ошибку в parser Исправили тесты

--- map_game/loader.py
+++ map_game/loader.py
@@ -1,43 +1,48 @@
 """
 Загрузка данных из xml файла
 """
+import click
+from collections import namedtuple
 import os
 import xml.etree.ElementTree as ET
-import click
 import map_game.database
 
 
-Node = namedtuple('Node', 'id lot let')
+Node = namedtuple('Node', 'id lon let')
 Way = namedtuple('Way', 'id nds tags')
+Tag = namedtuple('Tag', 'k v')
 
 
 def _parse_node(node):
     attrs = node.attrib
-    return Node(attrs['id'], attrs['lot'], attrs['lat'])
+    return Node(attrs['id'], attrs['lon'], attrs['lat'])
 
 
 def _parse_way(way):
+    global child, tagname
     attrs = way.attrib
     nid = attrs['id']
     nds = []
     tags = []
     for child in way:
         tagname = child.tag
-       attrs = child.tag
-       if tagname == 'nd'
+        attrs = child.tag
+        if tagname == 'nd':
            nds.append(attrs['ref'])
-       elif tagname == 'tag':
+        elif tagname == 'tag':
            tags.append(Tag(attrs['k'], attrs['v']))
     return  Way(nid, nds, tags)
 
 
 @click.command()
 @click.argument('filename')
-def load(filename:  str):
-    if os.path.exists(filename):
-        tree = ET.parse(filename)
-        return tree.getroot()
-    raise RuntimeError()
+def load(filename: str):
+    try:
+        db = map_game.database.create()
+        root = _load(filename)
+        db.save(parser(root))
+    except RuntimeError:
+        print(F'Файл  {filename} не найден')
 
 
 def parser(root) -> dict:
@@ -50,16 +55,10 @@ def parser(root) -> dict:
             node_info = parse_node(child)
             osm_info['nodes'].append(node_info)
         elif tagname == 'way':
-            way_info = parse_node(child)
+            way_info: object = parse_node(child)
             osm_info['nodes'].append(node_info)
     return osm_info
 
 
 if __name__ - - "__main__":
     filename = 'map.xml'
-    try:
-        db = map_game.database.create()
-        root = load(filename)
-        db.save(parser(root))
-    except RuntimeError:
-        print(F'Файл  {filename} не найден')
--- test/test_loader.py
+++ test/test_loader.py
@@ -5,10 +5,12 @@ import map_game
 
 def test_load_data():
     FILENAME = 'map.xml'
-    result = map_game.loader.load(FILENAME)
+    result = map_game.loader_load(FILENAME)
     assert result
 
-def test_parser(xml_object=None):
-    result = map_game.loader.parser(xml_object)
+def test_parser():
+    FILENAME = 'map.xml'
+    result = map_game.loader_load(FILENAME)
+    parsed = map_game.loader.parser(result)
     assert 'nodes' in result
     assert 'ways' in result
